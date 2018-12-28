540a784a1aabf64ad28eafb880aae4808a903c68
marioecg <marioecg@gmail.com>
2018-12-28 14:52:30 -0700

    removed smooth-scrollbar set up because of issues with waypoints

--- src/js/main.js
+++ src/js/main.js
@@ -1,73 +1,6 @@
 import "../scss/main.scss";
-import Scrollbar from "smooth-scrollbar";
 import anime from "animejs";
-
-/**
- * From Github issue on how to make anchor links using Smooth scrollbar
- * https://github.com/idiotWu/smooth-scrollbar/issues/128#issuecomment-390980479
- */
-class AnchorPlugin extends Scrollbar.ScrollbarPlugin {
-  static pluginName = "anchor";
-
-  onHashChange = () => {
-    this.handleHash(location.hash);
-  };
-
-  onClick = event => {
-    const { target } = event;
-
-    if (target.tagName !== "A") {
-      return;
-    }
-
-    const hash = target.getAttribute("href");
-
-    if (!hash || hash.charAt(0) !== "#") {
-      return;
-    }
-
-    this.handleHash(hash);
-  };
-
-  handleHash = hash => {
-    console.log("hash:", hash);
-
-    if (!hash) {
-      return;
-    }
-
-    if (hash === "#top") {
-      this.scrollbar.setMomentum(0, -this.scrollbar.scrollTop);
-    } else {
-      console.log("scrollTop:", this.scrollbar.containerEl.scrollTop);
-
-      this.scrollbar.scrollIntoView(document.querySelector(hash), {
-        offsetTop: -this.scrollbar.containerEl.scrollTop
-      });
-    }
-  };
-
-  onInit() {
-    this.handleHash(location.hash);
-
-    window.addEventListener("hashchange", this.onHashChange);
-
-    this.scrollbar.contentEl.addEventListener("click", this.onClick);
-  }
-
-  onDestory() {
-    window.removeEventListener("hashchange", this.onHashChange);
-
-    this.scrollbar.contentEl.removeEventListener("click", this.onClick);
-  }
-}
-
-Scrollbar.use(AnchorPlugin);
-
-// Init smooth scrollbar
-Scrollbar.init(document.querySelector("main"), {
-  damping: 0.1
-});
+import "waypoints/lib/noframework.waypoints.js";
 
 // Change language
 const body = document.querySelector("body");
@@ -145,3 +78,11 @@ function animateStart() {
       offset: "-=2500"
     });
 }
+
+// Animate using waypoints when a section is scrolled
+var about = new Waypoint({
+  element: document.getElementById("about"),
+  handler: function() {
+    console.log("hi!");
+  }
+});
