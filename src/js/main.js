8e9a57649e20b04e052fdf4db612a416a30cc913
marioecg <marioecg@gmail.com>
2018-12-27 17:42:42 -0700

    created animations for start and when switching language
    just for hero section

--- src/js/main.js
+++ src/js/main.js
@@ -70,16 +70,39 @@ Scrollbar.init(document.querySelector("main"), {
 });
 
 // Change language
+const body = document.querySelector("body");
+const wraps = document.querySelectorAll(
+  ".brand__tagline .wrap span, .nav__item .wrap span, .hero__title .wrap span, .hero__caption .wrap span"
+);
+
 function switchSpanish() {
-  const body = document.querySelector("body");
   body.className = "";
   body.classList.add("in-spanish");
+
+  wraps.forEach(wrap => {
+    anime.remove(wrap);
+
+    anime({
+      targets: wrap,
+      easing: "easeOutExpo",
+      translateY: "-50%"
+    });
+  });
 }
 
 function switchEnglish() {
-  const body = document.querySelector("body");
   body.className = "";
   body.classList.add("in-english");
+
+  wraps.forEach(wrap => {
+    anime.remove(wrap);
+
+    anime({
+      targets: wrap,
+      easing: "easeOutExpo",
+      translateY: 0
+    });
+  });
 }
 
 const esBtn = document.querySelector(".lang--es");
@@ -89,4 +112,36 @@ esBtn.addEventListener("click", switchSpanish);
 enBtn.addEventListener("click", switchEnglish);
 
 // Animations
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    animateStart();
+  }, 1250);
+});
+
+function animateStart() {
+  anime
+    .timeline({
+      easing: "easeOutExpo"
+    })
+    .add({
+      targets: ".hero__title .wrap span",
+      duration: 3000,
+      translateY: ["100%", 0],
+      delay: (el, i) => i * 125
+    })
+    .add({
+      targets:
+        ".brand__name .wrap a, .brand__tagline .wrap span, .langs .wrap span",
+      duration: 2000,
+      translateY: ["100%", 0],
+      delay: (el, i) => i * 50,
+      offset: "-=2500"
+    })
+    .add({
+      targets: ".nav__item .wrap span, .hero__caption .wrap span",
+      duration: 2000,
+      translateY: ["100%", 0],
+      delay: (el, i) => i * 50,
+      offset: "-=2500"
+    });
+}
